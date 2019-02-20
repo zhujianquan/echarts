4ea0035ee676d658f15f1685c771770ae623a5e9
zhujianquan <786056735@qq.com>
2019-02-20 20:30:33 +0800

    asdf

--- src/main/java/com/echarts/controller/UserController.java
+++ src/main/java/com/echarts/controller/UserController.java
@@ -25,6 +25,7 @@ public class UserController {
      * @return
      */
     @RequestMapping("/hello")
+
     public String helloHtml(HashMap<String, Object> map) {
         map.put("hello", "欢迎进入HTML页面");
         return "/index";
