da7bdfc9b19222b3e4572191e5998ea509f2722b
“zhujianquan <786056735@qq.com>
2019-03-19 14:11:01 +0800

    bootsctrap 引入

--- src/main/java/com/echarts/controller/UserController.java
+++ src/main/java/com/echarts/controller/UserController.java
@@ -30,10 +30,9 @@ public class UserController {
      * @return
      */
     @RequestMapping("/hello")
-
     public String helloHtml(HashMap<String, Object> map) {
         map.put("hello", "欢迎进入HTML页面");
-        return "/index";
+        return "redirect:/index.html";
     }
 
     @RequestMapping("/getUserCount")
